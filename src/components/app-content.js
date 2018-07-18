d1131c3dbc4ca29593296558060b99b94095c77c
fabiomagalhaes890 <fabiomagalhaes890@gmail.com>
2018-07-18 15:13:21 -0300

    Commit inicial

---
+++ src/components/app-content.js
@@ -0,0 +1,13 @@
+'use strict'
+
+import React from 'react'
+
+const AppContent = () => (
+  <div className='app'>
+    <div className='container-fluid'>
+      <button className='btn btn-sm btn-outline-success'>teste</button>
+    </div>
+  </div>
+)
+
+export default AppContent
